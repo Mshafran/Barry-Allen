6a59d7d9cf21076cb5950af7715b9bc9821856c5
Ryan Siu <rsiu@stuy.edu>
2017-03-09 12:57:07 -0500

    removed unnecessary stuff

--- QuickSort.java
+++ QuickSort.java
@@ -1,29 +1,11 @@
-// Ryan Siu
+// Team Barry-Allen -- Ryan Siu, Edwin Mok, Mark Shafran
 // APCS2 pd5
-// HW15 -- So So Quick
-// 2017-03-08
+// Lab 01 -- What Does the Data Say?
+// 2017-03-09
 
 /*****************************************************
  * class QuickSort
  * Implements quicksort algo to sort an array of ints in place
- *
- * 1. Summary of QuickSort algorithm:
- * QSort(arr): Run partition on an array, break the array into subarrays on
- *             either side of the number placed in the correct index, and 
- *             run partition on each of the subarrays. Repeat.
- *
- * 2a. Worst pivot choice / array state and associated runtime: 
- *     When the chosen pivot value is moved to either extreme of the array.
- *     O(n^2)
- *
- * 2b. Best pivot choice / array state and associated runtime: 
- *     When the chosen pivot value is moved to the middle of the array.
- *     O(nlogn)
- *
- * 3. Approach to handling duplicate values in array: 
- *    Duplicates are already handled by the partition method; they are put to
- *     the left of the partitioned array.
- *
  *****************************************************/
 
 public class QuickSort 
