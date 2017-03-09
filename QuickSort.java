bd749738fae9d8b8036739055f848f2a00ce3db4
Ryan Siu <rsiu@stuy.edu>
2017-03-09 11:27:25 -0500

    fixes

--- QuickSort.java
+++ QuickSort.java
@@ -35,32 +35,6 @@ public class QuickSort
 	o[x] = o[y];
 	o[y] = tmp;
     }
-
-    //print input array 
-    public static void printArr( int[] a ) {
-	for ( int o : a )
-	    System.out.print( o + " " );
-	System.out.println();
-    }
-
-    //shuffle elements of input array
-    public static void shuffle( int[] d ) {
-	int tmp;
-	int swapPos;
-	for( int i = 0; i < d.length; i++ ) {
-	    tmp = d[i];
-	    swapPos = i + (int)( (d.length - i) * Math.random() );
-	    swap( i, swapPos, d );
-	}
-    }
-
-    //return int array of size s, with each element fr range [0,maxVal)
-    public static int[] buildArray( int s, int maxVal ) {
-	int[] retArr = new int[s];
-	for( int i = 0; i < retArr.length; i++ )
-	    retArr[i] = (int)( maxVal * Math.random() );
-	return retArr;
-    }
     //--------------^  HELPER METHODS  ^--------------
 
 
@@ -83,7 +57,6 @@ public class QuickSort
     { 
 	if (left < right) {
 	    int pvtPos = partition( d, left, right, left); // last param doesn't matter
-	    System.out.println(pvtPos);
 	    qsort( d, left, pvtPos - 1);
 	    qsort( d, pvtPos + 1, right);
 	}
@@ -107,62 +80,5 @@ public class QuickSort
 
 	return s;
     }
-
-    //main method for testing
-    public static void main( String[] args ) 
-    {
-	
-	//get-it-up-and-running, static test case:
-	int [] arr1 = {7,1,5,12,3};
-	System.out.println("\narr1 init'd to: " );
-	printArr(arr1);
-
-	qsort( arr1 );	
-       	System.out.println("arr1 after qsort: " );
-	printArr(arr1);
-
-	// randomly-generated arrays of n distinct vals
-	int[] arrN = new int[10];
-	for( int i = 0; i < arrN.length; i++ )
-	    arrN[i] = i;
-       
-	System.out.println("\narrN init'd to: " );
-	printArr(arrN);
-
-       	shuffle(arrN);
-       	System.out.println("arrN post-shuffle: " );
-	printArr(arrN);
-
-	qsort( arrN );
-	System.out.println("arrN after sort: " );
-	printArr(arrN);
-
-	//get-it-up-and-running, static test case w/ dupes:
-	int [] arr2 = {7,1,5,12,3,7};
-	System.out.println("\narr2 init'd to: " );
-	printArr(arr2);
-
-	qsort( arr2 );	
-       	System.out.println("arr2 after qsort: " );
-	printArr(arr2);
-
-
-	// arrays of randomly generated ints
-	int[] arrMatey = new int[20];
-	for( int i = 0; i < arrMatey.length; i++ )
-	    arrMatey[i] = (int)( 48 * Math.random() );
-       
-	System.out.println("\narrMatey init'd to: " );
-	printArr(arrMatey);
-
-       	shuffle(arrMatey);
-       	System.out.println("arrMatey post-shuffle: " );
-	printArr(arrMatey);
-
-	qsort( arrMatey );
-	System.out.println("arrMatey after sort: " );
-	printArr(arrMatey);
-
-    }//end main
 
 }//end class QuickSort
--- QuickSortTester.java
+++ QuickSortTester.java
@@ -1,39 +1,15 @@
 /*======================================
-  class MergeSortTester
+  class QuickjortTester
 
-  ALGORITHM:
-  The merge sort algorithm is a divide-and-conquer method utilizing
-   recursion to sort an array. If the array length is one, then it is already 
-   sorted, so it is added to the merging block. Otherwise, the array is split
-   in half, and the halves are sorted and merged using the rsiu methodology.
-   Once the recursion is complete, there will be multiple nested merge 
-   calls, so the merges are merged until there is only one sorted array left.
+  TIMING MECHANISM:
+    
 
   BIG-OH CLASSIFICATION OF ALGORITHM:
-  O(nlogn) for most cases
-  O(n) if one element in array
 
-  Mean execution times for dataset of size n:
-  Batch size: 100
-  n=1        time: 0 ms
-  n=10       time: 0 ms
-  n=100      time: 0.02 ms
-  n=1000     time: 0.2 ms
-  n-5000     time: 0.86 ms
-  n=10000    time: 1.81 ms
-  n=50000    time: 8.64 ms
-  n=100000   time: 17.66 ms
-  n=500000   time: 107.49 ms
-  n=1000000  time: 236.34 ms
-  n-2500000  time: 600.08 ms
-  n=5000000  time: 1381.95 ms
-  n=7500000  time: 2176.85 ms
-  n=10000000 time: 2732.56 ms
 
   ANALYSIS:
-  MergeSort seems to run at linear logarithmic time. Most of the data points,
-  when plotted onto an Excel sheet and made into graphs, showed the strongest
-  correlation to a linear logarithmic runtime.
+
+  
   ======================================*/
 
 public class MergeSortTester 
@@ -68,7 +44,7 @@ public class MergeSortTester
 
 		// begin sort
 		long startTime = System.currentTimeMillis();
-		MergeSort.sort(arr);
+		QuickSort.qsort(arr);
 		long elapsedTime = System.currentTimeMillis() - startTime;
 
 		// O(n) operation for time reference
